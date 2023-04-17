68fc5a0b376c41e45b56b6d577accf13fe759b12
MauritzOrlinski <78754196+MauritzOrlinski@users.noreply.github.com>
2023-04-17 19:00:56 +0200

    Implementation of the iterative DFS.

--- src/main/java/com/jta/abialgorithms/graph/search/RecursiveDepthFirstSearch.java
+++ src/main/java/com/jta/abialgorithms/graph/search/RecursiveDepthFirstSearch.java
@@ -5,9 +5,9 @@ import com.jta.abialgorithms.datastructures.graph.Vertex;
 import com.jta.abialgorithms.datastructures.linear.List;
 
 /**
- * An implementation of the depth-first search algorithm.
+ * An implementation of the recursive depth-first search algorithm.
  */
-public final class DepthFirstSearch implements GraphSearchAlgorithm {
+public final class RecursiveDepthFirstSearch implements GraphSearchAlgorithm {
   @Override
   public Vertex search(final Graph graph, final Vertex vertex) {
     graph.setAllEdgeMarks(false);
