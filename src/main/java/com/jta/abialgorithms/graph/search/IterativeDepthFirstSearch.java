5dcc1d16502d621459f295d045f30e875ef87307
MauritzOrlinski <78754196+MauritzOrlinski@users.noreply.github.com>
2023-04-17 19:29:32 +0200

    Fixed indentation to match codebase

--- src/main/java/com/jta/abialgorithms/graph/search/IterativeDepthFirstSearch.java
+++ src/main/java/com/jta/abialgorithms/graph/search/IterativeDepthFirstSearch.java
@@ -9,32 +9,32 @@ import com.jta.abialgorithms.datastructures.linear.Stack;
  * An implementation of the iterative depth-first search algorithm.
  */
 public class IterativeDepthFirstSearch implements GraphSearchAlgorithm {
-    @Override
-    public Vertex search(final Graph graph, final Vertex vertex) {
-        Vertex startingVertex = FirstVertexRetriever.getFirstVertex(graph);
-        if (startingVertex == null) {
-            return null;
-        }
-        graph.setAllEdgeMarks(false);
-        Stack<Vertex> stack = new Stack<>();
-        stack.push(startingVertex);
-        while (!stack.isEmpty()) {
-            Vertex current = stack.top();
-            if (current.getID().equals(vertex.getID())) {
-                return current;
-            }
-            stack.pop();
-            current.setMark(true);
-            List<Vertex> neighbors = graph.getNeighbours(current);
-            neighbors.toFirst();
-            while (neighbors.hasAccess()) {
-                Vertex neighbor = neighbors.getContent();
-                if (!neighbor.isMarked()) {
-                    stack.push(neighbor);
-                }
-                neighbors.next();
-            }
-        }
-        return null;
+  @Override
+  public Vertex search(final Graph graph, final Vertex vertex) {
+    Vertex startingVertex = FirstVertexRetriever.getFirstVertex(graph);
+    if (startingVertex == null) {
+      return null;
     }
+    graph.setAllEdgeMarks(false);
+    Stack<Vertex> stack = new Stack<>();
+    stack.push(startingVertex);
+    while (!stack.isEmpty()) {
+      Vertex current = stack.top();
+      if (current.getID().equals(vertex.getID())) {
+        return current;
+      }
+      stack.pop();
+      current.setMark(true);
+      List<Vertex> neighbors = graph.getNeighbours(current);
+      neighbors.toFirst();
+      while (neighbors.hasAccess()) {
+        Vertex neighbor = neighbors.getContent();
+        if (!neighbor.isMarked()) {
+          stack.push(neighbor);
+        }
+        neighbors.next();
+      }
+    }
+    return null;
+  }
 }
